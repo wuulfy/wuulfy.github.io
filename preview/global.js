cbb7cafe2dae52d0d4b87be6f56aa2bbfe82b560
wuulfy <38586116+wuulfy@users.noreply.github.com>
2023-09-18 21:10:50 +0100

    Add files via upload

--- preview/global.js
+++ preview/global.js
@@ -1,22 +1,16 @@
 console.log("💗 I'm not sure what you're doing in the console but I hope you're having a nice day! ^_^")
 
-window.onscroll = function() {barDarken(); parallax()};
+window.onscroll = function() {barDarken()};
 
 function barDarken() {
-    if (document.body.scrollTop > 570 || document.documentElement.scrollTop > 570) {
-       document.getElementById("navContainer").style.width = "100%";
-        document.getElementById("navBar").style.width = "calc(100% - 73px)";
-        document.getElementById("navBar").style.margin = "10px 10px 0 0";
-        document.getElementById("navBar").style.padding = "0px 30px 0px 27px";
-        document.getElementById("navBar").style.borderRadius = "10px 10px 50x 50px";
-        document.getElementById("leftArea").style.margin = "10px 0px 0 25px";     
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        document.getElementById("navBar").style.margin = "10px 50px 0 0";
+        document.getElementById("navBlock").style.height = "90px";
+        document.getElementById("leftArea").style.margin = "5px 0px 0 25px";
       } else {
-        document.getElementById("navContainer").style.width = "1650px";
-        document.getElementById("navBar").style.width = "320px"; // WIDTH ELEMENT --- Hardcoded width is necessary for the animation to work
-        document.getElementById("navBar").style.margin = "40px 40px 0 0";
-        document.getElementById("navBar").style.padding = "0 6px 0 6px";
-        document.getElementById("navBar").style.borderRadius = "200px";
-        document.getElementById("leftArea").style.margin = "40px 0px 0 50px";
+        document.getElementById("navBar").style.margin = "40px 50px 0 0";
+        document.getElementById("navBlock").style.height = "0px";
+        document.getElementById("leftArea").style.margin = "36px 0px 0 25px";
       } 
     }
 
