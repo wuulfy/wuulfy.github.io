45e06e20831d8e62033bf2967f68a3b2413bc224
wuulfy <38586116+wuulfy@users.noreply.github.com>
2023-09-22 17:37:45 +0100

    Add files via upload

--- preview/global.js
+++ preview/global.js
@@ -1,14 +1,14 @@
-console.log("💗 I'm not sure what you're doing in the console but I hope you're having a nice day! ^_^")
+console.log("💗 I'm not sure what you're doing in the console but I hope you're having a nice day!")
 
 window.onscroll = function() {barDarken()};
 
 function barDarken() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        document.getElementById("navBar").style.margin = "10px 50px 0 0";
-        document.getElementById("navBlock").style.height = "90px";
-        document.getElementById("leftArea").style.margin = "5px 0px 0 25px";
+        document.getElementById("navBar").style.margin = "7px 50px 0 0";
+        document.getElementById("navBlock").style.height = "80px";
+        document.getElementById("leftArea").style.margin = "15px 0px 0 25px";
       } else {
-        document.getElementById("navBar").style.margin = "40px 50px 0 0";
+        document.getElementById("navBar").style.margin = "30px 50px 0 0";
         document.getElementById("navBlock").style.height = "0px";
         document.getElementById("leftArea").style.margin = "36px 0px 0 25px";
       } 
@@ -25,3 +25,15 @@ document.addEventListener("DOMContentLoaded", function(){
     elements[i].play();
   }
 });
+
+function mobileOpen() {
+  document.getElementById("mobileButtonContainer").style.display = "none";
+  document.getElementById("mobileUI").style.display = "block";
+  document.getElementById("closeRegion").style.display = "block";
+}
+
+function mobileClose() {
+  document.getElementById("mobileButtonContainer").style.display = "block";
+  document.getElementById("mobileUI").style.display = "none";
+  document.getElementById("closeRegion").style.display = "none";
+}
